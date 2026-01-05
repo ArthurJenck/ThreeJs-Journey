39964dbea6d4356122334fd34950bbd158a6339d
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-05 10:44:09 +0100

    replaced files with course documents

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-First Three.js Project
-Premier projet utilisant Three.js, dans le cadre du cours Three.js Journey de Bruno Simon.
+# Three.js Journey
+
+## Setup
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+``` bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+```
--- src/script.js
+++ src/script.js
@@ -1,38 +1,39 @@
-import * as THREE from "three"
+import * as THREE from 'three'
 
 // Canvas
-const canvas = document.querySelector("canvas#webgl")
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Object
+/**
+ * Objects
+ */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: "red" })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
 
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
-    height: 600,
+    height: 600
 }
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-
-Object.assign(camera.position, { z: 3 })
-
+camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas,
+    canvas: canvas
 })
-
 renderer.setSize(sizes.width, sizes.height)
-
 renderer.render(scene, camera)
-
-console.log(canvas)
