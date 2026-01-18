cdd751b257a50dd01891f5cb75fc64d7d5c6b13a
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-18 11:07:30 +0100

    textures starter

--- basics/src/script.js
+++ basics/src/script.js
@@ -1,25 +1,21 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import gsap from "gsap"
 import GUI from "lil-gui"
 
 /**
  * Debug
  */
 const gui = new GUI({
-  width: 250,
-  title: "Debug UI",
-  //   closeFolders: false,
+    title: "Debug UI",
 })
 
-// gui.close()
-// gui.hide()
-
+gui.close()
 window.addEventListener("keydown", (e) => {
-  if (e.key === "h") {
-    gui.show(gui._hidden)
-  }
+    if (e.key === "h") {
+        gui.show(gui._hidden)
+    }
 })
+gui.add(gui, "hide")
 
 const debugObject = {}
 
@@ -35,18 +31,14 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-debugObject.color = "#a778d8"
+debugObject.color = "#ff0000"
 
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({
-  color: debugObject.color,
-  wireframe: true,
-})
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 const cubeTweaks = gui.addFolder("Cube")
-// cubeTweaks.close()
 
 cubeTweaks.add(mesh.position, "y", -3, 3, 0.01).name("Elevation")
 cubeTweaks.add(mesh.position, "x").min(-3).max(3).step(0.01).name("Slide")
@@ -54,57 +46,38 @@ cubeTweaks.add(mesh.position, "z", -3, 3, 0.01).name("Depth")
 cubeTweaks.add(mesh, "visible").name("Show / Hide")
 cubeTweaks.add(material, "wireframe").name("Wireframe")
 cubeTweaks
-  .addColor(debugObject, "color")
-  .name("Color")
-  .onChange((newColor) => {
-    material.color.set(debugObject.color)
-  })
+    .addColor(debugObject, "color")
+    .name("Color")
+    .onChange((newColor) => {
+        material.color.set(debugObject.color)
+    })
 
 debugObject.spin = () => {
-  gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
+    gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 })
 }
 
 cubeTweaks.add(debugObject, "spin")
-
-debugObject.subdivision = 2
-
-cubeTweaks
-  .add(debugObject, "subdivision")
-  .min(1)
-  .max(20)
-  .step(1)
-  .onFinishChange(() => {
-    mesh.geometry.dispose()
-    mesh.geometry = new THREE.BoxGeometry(
-      1,
-      1,
-      1,
-      debugObject.subdivision,
-      debugObject.subdivision,
-      debugObject.subdivision
-    )
-  })
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 }
 
 window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -112,14 +85,14 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100,
 )
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
@@ -130,7 +103,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -141,16 +114,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()
 
-  // Update controls
-  controls.update()
+    // Update controls
+    controls.update()
 
-  // Render
-  renderer.render(scene, camera)
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
 tick()
