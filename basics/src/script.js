e73e6cf27df6f76a20e9a88e4932f6aa8cbd43a7
Arthur <arthurjenckdev@gmail.com>
2026-01-19 18:16:17 +0100

    played with mesh physical material

--- basics/src/script.js
+++ basics/src/script.js
@@ -82,28 +82,85 @@ const objectsFolder = gui.addFolder('Objects & Material')
 // material.gradientMap = gradientTexture
 
 // MeshStandardMaterial
-const material = new THREE.MeshStandardMaterial()
+// const material = new THREE.MeshStandardMaterial()
 // material.metalness = 1
 // material.roughness = 0
-material.metalness = 1
-material.roughness = 1
+// material.metalness = 1
+// material.roughness = 1
+// objectsFolder.add(material, 'metalness').min(0).max(1).step(0.0001).name("Metalness")
+// objectsFolder.add(material, 'roughness').min(0).max(1).step(0.0001).name("Roughness")
+
+// material.side = THREE.DoubleSide
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// objectsFolder.add(material, 'aoMapIntensity').min(0).max(1).step(0.001).name('AoMapIntensity')
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.1
+// objectsFolder.add(material, 'displacementScale').min(0).max(1).step(0.001).name('DisplacementScale')
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+// MeshPhysicalMaterial
+const material = new THREE.MeshPhysicalMaterial()
+material.metalness = 0
+material.roughness = 0
 objectsFolder.add(material, 'metalness').min(0).max(1).step(0.0001).name("Metalness")
 objectsFolder.add(material, 'roughness').min(0).max(1).step(0.0001).name("Roughness")
 
-material.side = THREE.DoubleSide
-material.map = doorColorTexture
-material.aoMap = doorAmbientOcclusionTexture
-material.aoMapIntensity = 1
-objectsFolder.add(material, 'aoMapIntensity').min(0).max(1).step(0.001).name('AoMapIntensity')
-material.displacementMap = doorHeightTexture
-material.displacementScale = 0.1
-objectsFolder.add(material, 'displacementScale').min(0).max(1).step(0.001).name('DisplacementScale')
-material.metalnessMap = doorMetalnessTexture
-material.roughnessMap = doorRoughnessTexture
-material.normalMap = doorNormalTexture
-material.normalScale.set(0.5, 0.5)
-material.transparent = true
-material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// objectsFolder.add(material, 'aoMapIntensity').min(0).max(1).step(0.001).name('AoMapIntensity')
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.1
+// objectsFolder.add(material, 'displacementScale').min(0).max(1).step(0.001).name('DisplacementScale')
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+// Clearcoat
+// material.clearcoat = 1
+// material.clearcoatRoughness = 0
+
+// objectsFolder.add(material, 'clearcoat').min(0).max(1).step(0.0001)
+// objectsFolder.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+
+// Sheen
+// material.sheen = 1
+// material.sheenRoughness = 0.25
+// material.sheenColor.set(1, 1, 1)
+
+// objectsFolder.add(material, 'sheen').min(0).max(1).step(0.0001)
+// objectsFolder.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
+// objectsFolder.addColor(material, 'sheenColor')
+
+// Iridescence
+// material.iridescence = 1
+// material.iridescenceIOR = 1
+// material.iridescenceThicknessRange = [100, 800]
+
+// objectsFolder.add(material, "iridescence").min(0).max(1).step(0.0001)
+// objectsFolder.add(material, 'iridescenceIOR').min(0).max(2.333).step(0.0001)
+// objectsFolder.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
+// objectsFolder.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)
+
+// Transmission
+material.transmission = 1
+material.ior = 1.5
+material.thickness = 0.5
+
+objectsFolder.add(material, 'transmission').min(0).max(1).step(0.0001)
+objectsFolder.add(material, 'ior').min(1).max(10).step(0.0001)
+objectsFolder.add(material, 'thickness').min(0).max(1).step(0.0001)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material)
 
