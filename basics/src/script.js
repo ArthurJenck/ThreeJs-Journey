95ec547e3bd0dc977ba4ab0ca1c06c672bd1b594
Arthur <arthurjenckdev@gmail.com>
2026-01-20 10:52:00 +0100

    3d text starter

--- basics/src/script.js
+++ basics/src/script.js
@@ -1,18 +1,13 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
-
-/**
- * Debug UI
- */
-const gui = new GUI({
-    title: 'Debug UI'
-})
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI()
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -21,180 +16,18 @@ const scene = new THREE.Scene()
 
 /**
  * Textures
-*/
-
+ */
 const textureLoader = new THREE.TextureLoader()
 
-const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-
-const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
-
-const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
-
-doorColorTexture.colorSpace = THREE.SRGBColorSpace
-matcapTexture.colorSpace = THREE.SRGBColorSpace
-
 /**
- * Objects
-*/
-const objectsFolder = gui.addFolder('Objects & Material')
-// MeshBasicMaterial
-// const material = new THREE.MeshBasicMaterial()
-// material.map = doorColorTexture
-// material.color = new THREE.Color('red')
-// material.wireframe = true
-// material.opacity = 0.5
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-// material.side = THREE.DoubleSide
-
-// MeshNormalMaterial
-// const material = new THREE.MeshNormalMaterial()
-// material.flatShading = true
-
-// MeshMatcapMaterial
-// const material = new THREE.MeshMatcapMaterial()
-// material.matcap = matcapTexture
-// material.flatShading = true
-
-// MeshDepthMaterial
-// const material = new THREE.MeshDepthMaterial()
-
-// MeshLambertMaterial
-// const material = new THREE.MeshLambertMaterial()
-
-// MeshPhongMaterial
-// const material = new THREE.MeshPhongMaterial()
-// material.shininess = 100
-// material.specular = new THREE.Color('white')
-
-// MeshToonMaterial
-// const material = new THREE.MeshToonMaterial()
-// gradientTexture.minFilter = THREE.NearestFilter
-// gradientTexture.magFilter = THREE.NearestFilter
-// gradientTexture.generateMipmaps = false
-// material.gradientMap = gradientTexture
-
-// MeshStandardMaterial
-// const material = new THREE.MeshStandardMaterial()
-// material.metalness = 1
-// material.roughness = 0
-// material.metalness = 1
-// material.roughness = 1
-// objectsFolder.add(material, 'metalness').min(0).max(1).step(0.0001).name("Metalness")
-// objectsFolder.add(material, 'roughness').min(0).max(1).step(0.0001).name("Roughness")
-
-// material.side = THREE.DoubleSide
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.aoMapIntensity = 1
-// objectsFolder.add(material, 'aoMapIntensity').min(0).max(1).step(0.001).name('AoMapIntensity')
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.1
-// objectsFolder.add(material, 'displacementScale').min(0).max(1).step(0.001).name('DisplacementScale')
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.normalScale.set(0.5, 0.5)
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-// MeshPhysicalMaterial
-const material = new THREE.MeshPhysicalMaterial()
-material.metalness = 0
-material.roughness = 0
-objectsFolder.add(material, 'metalness').min(0).max(1).step(0.0001).name("Metalness")
-objectsFolder.add(material, 'roughness').min(0).max(1).step(0.0001).name("Roughness")
-
-// material.side = THREE.DoubleSide
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.aoMapIntensity = 1
-// objectsFolder.add(material, 'aoMapIntensity').min(0).max(1).step(0.001).name('AoMapIntensity')
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.1
-// objectsFolder.add(material, 'displacementScale').min(0).max(1).step(0.001).name('DisplacementScale')
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.normalScale.set(0.5, 0.5)
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-// Clearcoat
-// material.clearcoat = 1
-// material.clearcoatRoughness = 0
-
-// objectsFolder.add(material, 'clearcoat').min(0).max(1).step(0.0001)
-// objectsFolder.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
-
-// Sheen
-// material.sheen = 1
-// material.sheenRoughness = 0.25
-// material.sheenColor.set(1, 1, 1)
-
-// objectsFolder.add(material, 'sheen').min(0).max(1).step(0.0001)
-// objectsFolder.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
-// objectsFolder.addColor(material, 'sheenColor')
-
-// Iridescence
-// material.iridescence = 1
-// material.iridescenceIOR = 1
-// material.iridescenceThicknessRange = [100, 800]
-
-// objectsFolder.add(material, "iridescence").min(0).max(1).step(0.0001)
-// objectsFolder.add(material, 'iridescenceIOR').min(0).max(2.333).step(0.0001)
-// objectsFolder.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
-// objectsFolder.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)
-
-// Transmission
-material.transmission = 1
-material.ior = 1.5
-material.thickness = 0.5
-
-objectsFolder.add(material, 'transmission').min(0).max(1).step(0.0001)
-objectsFolder.add(material, 'ior').min(1).max(10).step(0.0001)
-objectsFolder.add(material, 'thickness').min(0).max(1).step(0.0001)
-
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material)
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1, 100, 100), material)
-
-const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 64, 128), material)
-
-sphere.position.x = -1.5
-torus.position.x = 1.5
-
-scene.add(sphere, plane, torus)
-
-/**
- * Lights
+ * Object
  */
-// const ambientLight = new THREE.AmbientLight('white', 1)
-// scene.add(ambientLight)
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial()
+)
 
-// const pointLight = new THREE.PointLight('white', 30)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
-
-/**
- * Environment Map
- */
-const rgbeLoader = new RGBELoader()
-rgbeLoader.load('/textures/environmentMap/2k.hdr', (environmentMap) => {
-    environmentMap.mapping = THREE.EquirectangularReflectionMapping
-
-    scene.background = environmentMap
-    scene.environment = environmentMap
-})
+scene.add(cube)
 
 /**
  * Sizes
@@ -204,7 +37,8 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () =>
+{
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -220,7 +54,7 @@ window.addEventListener('resize', () => {
 
 /**
  * Camera
-*/
+ */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
@@ -246,17 +80,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () => {
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
-
-    sphere.rotation.x = elapsedTime * -0.15
-    sphere.rotation.y = elapsedTime * 0.1
-
-    plane.rotation.x = elapsedTime * -0.15
-    plane.rotation.y = elapsedTime * 0.1
-
-    torus.rotation.x = elapsedTime * -0.15
-    torus.rotation.y = elapsedTime * 0.1
 
     // Update controls
     controls.update()
