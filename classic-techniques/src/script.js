937c2f9aed91493a69f5430be552b8c5151fb931
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-22 17:26:59 +0100

    indent

--- classic-techniques/src/script.js
+++ classic-techniques/src/script.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import GUI from 'lil-gui'
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import GUI from "lil-gui"
 
 /**
  * Base
@@ -9,7 +9,7 @@ import GUI from 'lil-gui'
 const gui = new GUI()
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
@@ -19,16 +19,16 @@ const scene = new THREE.Scene()
  */
 // Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
+gui.add(ambientLight, "intensity").min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
-directionalLight.position.set(2, 2, - 1)
-gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
-gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
-gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
-gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+directionalLight.position.set(2, 2, -1)
+gui.add(directionalLight, "intensity").min(0).max(3).step(0.001)
+gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001)
+gui.add(directionalLight.position, "y").min(-5).max(5).step(0.001)
+gui.add(directionalLight.position, "z").min(-5).max(5).step(0.001)
 scene.add(directionalLight)
 
 /**
@@ -36,23 +36,17 @@ scene.add(directionalLight)
  */
 const material = new THREE.MeshStandardMaterial()
 material.roughness = 0.7
-gui.add(material, 'metalness').min(0).max(1).step(0.001)
-gui.add(material, 'roughness').min(0).max(1).step(0.001)
+gui.add(material, "metalness").min(0).max(1).step(0.001)
+gui.add(material, "roughness").min(0).max(1).step(0.001)
 
 /**
  * Objects
  */
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    material
-)
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(5, 5),
-    material
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.5
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
+plane.rotation.x = -Math.PI * 0.5
+plane.position.y = -0.5
 
 scene.add(sphere, plane)
 
@@ -61,11 +55,10 @@ scene.add(sphere, plane)
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener("resize", () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -83,7 +76,12 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100,
+)
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
@@ -97,7 +95,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -107,8 +105,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
