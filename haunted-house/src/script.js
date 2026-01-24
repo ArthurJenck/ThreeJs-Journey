dd5437694a429a2a58af93a3392dc152f4e2cdda
Arthur Jenck <arthurjenckdev@gmail.com>
2026-01-24 21:23:17 +0100

    changed floor texture

--- haunted-house/src/script.js
+++ haunted-house/src/script.js
@@ -25,8 +25,11 @@ const floorFolder = gui.addFolder('Floor')
 
 const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg')
 
+// const floorColorTexture = textureLoader.load(
+//     './floor/coast_sand_rocks_02_1k/diff.jpg'
+// )
 const floorColorTexture = textureLoader.load(
-    './floor/coast_sand_rocks_02_1k/diff.jpg'
+    './floor/aerial_rocks_02_1k/diff.jpg'
 )
 floorColorTexture.repeat.set(8, 8)
 floorColorTexture.wrapS = THREE.RepeatWrapping
@@ -34,22 +37,29 @@ floorColorTexture.wrapT = THREE.RepeatWrapping
 
 floorColorTexture.colorSpace = THREE.SRGBColorSpace
 
-const floorARMTexture = textureLoader.load(
-    './floor/coast_sand_rocks_02_1k/arm.jpg'
-)
+// const floorARMTexture = textureLoader.load(
+//     './floor/coast_sand_rocks_02_1k/arm.jpg'
+// )
+const floorARMTexture = textureLoader.load('./floor/aerial_rocks_02_1k/arm.jpg')
 floorARMTexture.repeat.set(8, 8)
 floorARMTexture.wrapS = THREE.RepeatWrapping
 floorARMTexture.wrapT = THREE.RepeatWrapping
 
+// const floorNormalTexture = textureLoader.load(
+//     './floor/coast_sand_rocks_02_1k/nor_gl.jpg'
+// )
 const floorNormalTexture = textureLoader.load(
-    './floor/coast_sand_rocks_02_1k/nor_gl.jpg'
+    './floor/aerial_rocks_02_1k/nor_gl.jpg'
 )
 floorNormalTexture.repeat.set(8, 8)
 floorNormalTexture.wrapS = THREE.RepeatWrapping
 floorNormalTexture.wrapT = THREE.RepeatWrapping
 
+// const floorDisplacementTexture = textureLoader.load(
+//     './floor/coast_sand_rocks_02_1k/disp.jpg'
+// )
 const floorDisplacementTexture = textureLoader.load(
-    './floor/coast_sand_rocks_02_1k/disp.jpg'
+    './floor/aerial_rocks_02_1k/disp.jpg'
 )
 floorDisplacementTexture.repeat.set(8, 8)
 floorDisplacementTexture.wrapS = THREE.RepeatWrapping
